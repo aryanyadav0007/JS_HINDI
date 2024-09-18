eb633c53624a3323b2669bb77572b48ec683508f
aryanyadav0007 <164308239+aryanyadav0007@users.noreply.github.com>
2024-09-18 09:19:08 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId =144553
+var accountEmail ="aryan@gmail.com"
+let accountPass ="aryan@123"
+
+let accountCity ="jaipur"
+//accountId =20
+
+var accountEmail="work@gmail.com"
+
+//console.log(accountId)
+console.table([accountId, accountEmail,accountPass,accountCity])
