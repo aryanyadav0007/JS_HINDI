8e877f524005eac8b9c463c353cb4c4a3215f8c0
aryanyadav0007 <164308239+aryanyadav0007@users.noreply.github.com>
2024-09-20 15:30:43 +0000

    dataTypes

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -3,9 +3,15 @@ var accountEmail ="aryan@gmail.com"
 let accountPass ="aryan@123"
 
 let accountCity ="jaipur"
-//accountId =20
+//accountPass=20
 
 var accountEmail="work@gmail.com"
+
+
+/*prefer not to use var 
+becuase of block space and funtional space
+*/
+
 
 //console.log(accountId)
 console.table([accountId, accountEmail,accountPass,accountCity])
