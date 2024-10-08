e2ced10e7dbf6e02e321823dbae6ab75b1f5e357
aryanyadav0007 <164308239+aryanyadav0007@users.noreply.github.com>
2024-10-08 13:57:50 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,26 @@
+const name ="aryan"
+const repoCount = 50
+
+//console.log(name + repoCount + " value")
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('aryan')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('a'));
+ 
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5,3)
+console.log(anotherString);
+
+const newstringOne = "    hello-world    ";
+console.log(newstringOne);
+console.log(newstringOne.trim());
+ 
