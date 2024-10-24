5956da4a3d7e7892797040a4962de12db41bbb8a
aryanyadav0007 <164308239+aryanyadav0007@users.noreply.github.com>
2024-10-24 19:16:58 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,24 @@
+const myarr =[1,2,3,4,5]
+
+console.log(myarr);
+myarr.push(6)
+myarr.pop()
+
+//console.log(myarr.includes(3));
+
+//myarr.unshift(9)
+//myarr.shift(2)
+
+const newarr = myarr.join();
+console.log(newarr)
+console.log(myarr)
+
+//slice and splice
+
+const myn1 = myarr.slice(1,3)
+console.log(myarr)
+console.log(myn1) //slice does not changed the original array
+
+const myn2 = myarr.splice(1,3);
+console.log(myarr)
+console.log(myn2) //splice changes the original array
